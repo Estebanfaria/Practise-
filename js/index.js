54a04d5177184c0c0aea008ad13fca1912745dd1
Esteban Faria <tebifaria@gmail.com>
2022-11-30 18:22:11 -0300

    estructura css

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// let productos = 0;
+
+// const cantidadProductos = () => {
+//     let productos = parseInt(prompt('ingrese la cantidad de productos'))
+
+// }
+
+// function sumar(){
+    
+// }
+
+// cantidadProductos();
+
+
+
+
